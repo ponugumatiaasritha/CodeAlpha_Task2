88b1df5539487ad5df06a63eac7a2d510178a1a3
ponugumatiaasritha <ponugumatiaasritha@gmail.com>
2026-08-09 15:15:18 +0530

    Add files via upload

---
+++ chatbox.py
@@ -0,0 +1,29 @@
+# Basic Rule-Based Chatbot
+
+def chatbot_response(user_input):
+    user_input = user_input.lower()
+
+    if user_input == "hello":
+        return "Hi!"
+
+    elif user_input == "how are you":
+        return "I'm fine, thanks!"
+
+    elif user_input == "bye":
+        return "Goodbye!"
+
+    else:
+        return "Sorry, I don't understand that."
+
+
+print("Chatbot: Hello! Type 'bye' to exit.")
+
+while True:
+    user_input = input("You: ")
+
+    response = chatbot_response(user_input)
+
+    print("Chatbot:", response)
+
+    if user_input.lower() == "bye":
+        break
